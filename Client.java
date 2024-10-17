4e5ac7d25fa45159366d1d37b6b752d2175af9f4
Saroj Shrestha <saroj.shr985@gmail.com>
2024-10-16 19:24:39 -0600

    commited client.java

---
+++ Client.java
@@ -0,0 +1,25 @@
+
+public interface Client {
+	void webDesign();
+	void webDevelopment();
+}
+
+abstract class Designer implements Client{
+	public void webDesign() {
+		System.out.println("Design is done.");
+	}
+}
+
+class Dev extends Designer{
+	public void webDevelopment() {
+		System.out.println("Development is done.");
+	}
+}
+
+class check {
+	public static void main(String[] args) {
+		Dev d = new Dev();
+		d.webDesign();
+		d.webDevelopment();
+	}
+}
